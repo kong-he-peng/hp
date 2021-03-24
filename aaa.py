d11b1fc37b0403007e50cec0d22ff81294f3d976
嘿-宝贝儿 <kongfuzi_k@163.com>
2021-03-24 11:42:17 +0800

    添加了一个py文件

---
+++ aaa.py
@@ -0,0 +1,30 @@
+improt os
+improt time 
+from time improt time 
+improt sys
+
+
+
+
+
+print("今天的天气真不错")
+print("也不是很热，非常适合我")
+print("还算可以")
+
+
+
+def func():
+    return 1+1
+
+def func1():
+    return 2+2
+
+def func2():
+    x = 1
+    y = 2
+    return x+=y
+
+
+print(func())
+print(func1())
+print(func2())
