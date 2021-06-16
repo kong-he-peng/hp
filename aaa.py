6d3668b907b69e047ad92f1afacde815e90527db
hepeng <kongfuzi_k@163.com>
2021-06-16 11:04:03 +0800

    合并了一个文件

--- aaa.py
+++ aaa.py
@@ -1,4 +1,30 @@
-今天天气不错啊
-你觉得呢
-明天背小书包
-是不是
+improt os
+improt time 
+from time improt time 
+improt sys
+
+
+
+
+
+print("今天的天气真不错")
+print("也不是很热，非常适合我")
+print("还算可以")
+
+
+
+def func():
+    return 1+1
+
+def func1():
+    return 2+2
+
+def func2():
+    x = 1
+    y = 2
+    return x+=y
+
+
+print(func())
+print(func1())
+print(func2())
